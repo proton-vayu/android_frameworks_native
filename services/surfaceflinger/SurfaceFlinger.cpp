aa864aa5fbca133dd815768021b463f700683f5d
Lars Svensson <lars.svensson@sony.com>
2021-12-21 07:41:57 +0100

    Ensure deferred cleanup when display is off
    Using DRM video playback app and wi-fi direct, allocation of
    AutoBackendTexture objects sometimes continue for some time after the
    display is off. Clean-up of these is deferred until postRender() is
    called, but only invoked if the display is on. This eventually leads
    to depletion of secure memory resources and a fatal assert.
    
    This ensures cleanupPostRender() is called regardless of display
    state.
    
    Test: Play Movies with screen mirroring, repeatedly sleep/wake device
    Change-Id: Ib0d530582e124984ef70f91f6e398f62c2312f4c

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2403,6 +2403,7 @@ void SurfaceFlinger::postComposition() {
     mTimeStats->recordDisplayEventConnectionCount(sfConnections + appConnections);
 
     if (isDisplayConnected && !display->isPoweredOn()) {
+        getRenderEngine().cleanupPostRender();
         return;
     }
 
